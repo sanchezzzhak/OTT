3d39b979412a29066ec7b61f832e03a82339efe8
Tutik Alexsandr <sanchezzzhak@ya.ru>
2022-02-01 00:47:14 +0300

    feat(backend) create small jwt helper

--- frontend/App.vue
+++ frontend/App.vue
@@ -12,21 +12,7 @@
 
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-.nav-links {
-  a {
-    margin: 0 2px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
 
 <script>
 export default {
