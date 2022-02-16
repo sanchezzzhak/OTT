3204275c42c808ffb6bbd426d567310bd6dbc5f5
sanchezzzhak <sanchezzzhak@ya.ru>
2022-02-16 14:02:04 +0300

    feat(backend) update jsdocs

--- backend/controllers/abstract-controller.js
+++ backend/controllers/abstract-controller.js
@@ -2,6 +2,13 @@ const ejs = require('ejs');
 const fs = require('fs');
 const {getMime} = require('./../utils/mime');
 
+/**
+ * callback async read post data for chunks
+ * @param res
+ * @param cb
+ * @param err
+ * @private
+ */
 const __readBody = (res, cb, err) => {
   let buffer;
   /* Register data cb */
@@ -9,24 +16,24 @@ const __readBody = (res, cb, err) => {
     let chunk = Buffer.from(ab);
     if (isLast) {
       if (buffer) {
-        cb(Buffer.concat([buffer, chunk]))
+        cb(Buffer.concat([buffer, chunk]));
         return;
       }
       cb(chunk);
       return;
     }
-
+    
     if (buffer) {
       buffer = Buffer.concat([buffer, chunk]);
       return;
     }
     buffer = Buffer.concat([chunk]);
     return;
-  })
-
+  });
+  
   /* Register error cb */
   res.onAborted(err);
-}
+};
 
 class AbstractController {
   format = 'html';
@@ -37,70 +44,86 @@ class AbstractController {
     this.req = opts.req;
     this.res = opts.res;
     this.route = opts.route ?? {};
-
+    
     this.res.onAborted(() => {
       this.res.aborted = true;
     });
   }
-
-  setCorsHeaders() {
-    this.res.writeHeader("Access-Control-Allow-Origin", "*");
-    this.res.writeHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    this.res.writeHeader("Access-Control-Allow-Headers", "origin, content-type, accept, x-requested-with");
-    this.res.writeHeader("Access-Control-Max-Age", "3600");
-  }
-
-  compactErrors(listErrors){
+  
+  /**
+   * remove unnecessary information from the validators from the array
+   * @param listErrors
+   * @returns {[]}
+   */
+  compactErrors(listErrors) {
     const errors = [];
     listErrors.map((error) => {
       let {field, message} = error;
       errors.push({field, message});
-    })
+    });
     return errors;
   }
   
-  asJson(obj, statusCode = 200){
+  /**
+   * output server content (format json)
+   * @param {{}} obj
+   * @param {number} statusCode
+   */
+  asJson(obj, statusCode = 200) {
     this.renderRaw({view: JSON.stringify(obj), statusCode, format: 'json'});
   }
-
+  
+  /**
+   * final response to the server
+   * @param context
+   */
   end(context) {
     if (!this.res.aborted) {
       this.res.end(context);
     }
   }
-
+  
+  /**
+   * request to microservices models, while for post requests
+   * @param {string} actionName
+   * @returns {Promise<void>}
+   */
   async callRestAction(actionName) {
     if (this.req.getMethod() === 'options') {
       this.end('');
       return;
     }
-
+    
     try {
       let content = await this.readBody();
       let json = JSON.parse(content.toString());
-
+      
       try {
-        let response = await this.broker.call(actionName, json)
+        let response = await this.broker.call(actionName, json);
         return this.asJson(response);
-      } catch (e){
+      } catch (e) {
         if (e.type === 'VALIDATION_ERROR') {
           const errors = this.compactErrors(e.data);
-          return this.asJson({err: 'Validation error', errors})
+          return this.asJson({err: 'Validation error', errors});
         }
       }
     } catch (e) {
       this.broker.logger.error(e);
     }
-
+    
     return this.asJson({err: 'Invalid JSON format'}, 403);
   }
-
+  
+  /**
+   * read post data in body
+   * @returns {Promise<unknown>}
+   */
   readBody() {
     return new Promise((resolve, reject) => {
-      __readBody(this.res, resolve, reject)
-    })
+      __readBody(this.res, resolve, reject);
+    });
   }
-
+  
   /**
    * output server content
    * @param {string} view
@@ -112,7 +135,8 @@ class AbstractController {
       format = this.format;
     }
     if (format) {
-      !this.res.aborted && this.res.writeHeader('content-type', getMime('.' + format));
+      !this.res.aborted &&
+      this.res.writeHeader('content-type', getMime('.' + format));
     }
     if (statusCode === void 0) {
       statusCode = this.statusCode;
@@ -122,20 +146,20 @@ class AbstractController {
     }
     this.end(view);
   }
-
+  
   /**
    * output server content (prepare ejs template+params)
-   * @param {string} template
-   * @param {*} params
-   * @param {string|number|null} statusCode
-   * @param {string|null} format
+   * @param template
+   * @param params
+   * @param statusCode
+   * @param format
    */
   render({template, params, statusCode, format} = {}) {
     this.renderRaw({
       view: ejs.render(template, params), statusCode, format,
     });
   }
-
+  
 }
 
 module.exports = AbstractController;
--- backend/controllers/traffic-controller.js
+++ backend/controllers/traffic-controller.js
@@ -2,13 +2,25 @@ const AbstractController = require('./abstract-controller');
 const {randomUUID} = require('crypto');
 
 class TrafficController extends AbstractController {
+  
+  /**
+   * main method for analytics
+   * result traffic id for fist request
+   */
   index() {
     let id = this.req.getParameter(0);
     let data = JSON.stringify({
       trafficId: randomUUID(), // this.broker.generateUid()
     });
     this.renderRaw({view: `window['_ott_${id}'] = ${data}`, format: 'js'});
-    
+  }
+  
+  /**
+   * aggregate traffic by trafficId save to buffer table
+   * for further analysis
+   */
+  log() {
+    // todo adds - save traffic for buffer table
   }
   
 }
