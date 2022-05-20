5fdc5af167332b62b276f65b0584429a4067f7fd
sanchezzzhak <sanchezzzhak@ya.ru>
2022-05-20 15:38:59 +0300

    fix(frontend) fixed sidebar menu and update color link

--- frontend/components/Sidebar.vue
+++ frontend/components/Sidebar.vue
@@ -2,8 +2,8 @@
   <div class="navigation--sidebar">
     <nav class="navigation--block" v-for="item in items">
       <router-link class="navigation--link" :to="item.to">
-        <div class="icon" v-if="item.icon">{{ item.icon }}</div>
-        <span>{{ item.label }}</span>
+          <i class="icon" v-if="item.icon" :class="item.icon"></i>
+          <span class="label">{{item.label}}</span>
       </router-link>
     </nav>
   </div>
@@ -12,7 +12,8 @@
 export default {
   data: () => ({
     items: [
-      {label: 'Stats', to: {path: '/dashboard/stat'}},
+      {label: 'Stats', to: {path: '/stat'}, icon: 'fa-solid fa-chart-line'},
+      {label: 'Settings', to: {path: '/settings'}, icon: 'fa-solid fa-gear'},
     ],
   }),
   name: "sidebar",
