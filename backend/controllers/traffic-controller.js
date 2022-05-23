5087c2e9cac4a077b1c1f2a1fd63ef8600f09256
sanchezzzhak <sanchezzzhak@ya.ru>
2022-05-23 17:51:59 +0300

    feat(backend) basic interaction between micro-services while saving traffic

--- backend/controllers/traffic-controller.js
+++ backend/controllers/traffic-controller.js
@@ -1,25 +1,75 @@
 const AbstractController = require('./abstract-controller');
 const {randomUUID} = require('crypto');
 
+const EVENTS = {
+  HIT: 1,         // the very first click or lucky, start started
+  HEAT: 7         // event for heat map graph
+};
+
 class TrafficController extends AbstractController {
   
   /**
    * main method for analytics
    * result traffic id for fist request
    */
-  index() {
+  async index() {
+    this.setCorsHeadersOver();
+    this.setClientHintsHeaders();
+  
+    let trafficId = randomUUID();
+    let result = await this.#pushBuffer()
+    
     let id = this.req.getParameter(0);
     let data = JSON.stringify({
-      trafficId: randomUUID(), // this.broker.generateUid()
+      trafficId, // this.broker.generateUid()
     });
+    
     this.renderRaw({view: `window['_ott_${id}'] = ${data}`, format: 'js'});
   }
+  
+  /**
+   * get device detect for
+   * @returns {Promise<void>}
+   */
+  async deviceInfo() {
+    let useragent = this.getUserAgent();
+    let headers = this.getClientHintHeaders();
+  
+    return await this.broker.callRestAction('detector.detect', {
+      useragent, headers
+    });
+  }
+  
+  /**
+   * save traffic data for buffer table
+   *
+   * @param params
+   * @returns {Promise<null|void>}
+   */
+  async #pushBuffer(params = {}) {
+    let eventId = params.event ?? 0;
+    if (!eventId) {
+      return null;
+    }
+    
+    // detect device only for EVENTS.HIT
+    let deviceInfo = {};
+    if (eventId === EVENTS.HIT) {
+      deviceInfo = await this.deviceInfo();
+    }
+
+    return await this.broker.callRestAction('stat.buffer', params);
+  }
+  
   
   /**
    * aggregate traffic by trafficId save to buffer table
    * for further analysis
    */
-  log() {
+  async log() {
+    this.setCorsHeadersOver();
+    this.setClientHintsHeaders();
+    
     // todo adds - save traffic for buffer table
   }
   
