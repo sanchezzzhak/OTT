ab6079dd165a3d3454ef514274a778efc85a9a66
Tutik Alexsandr <sanchezzzhak@ya.ru>
2022-07-17 09:54:09 +0300

    feat(backend) create project service + tablet

---
+++ backend/services/models/project.service.js
@@ -0,0 +1,82 @@
+const {Service} = require('moleculer');
+const DbService = require('moleculer-db');
+const SqlAdapter = require('moleculer-db-adapter-sequelize');
+const Sequelize = require('sequelize');
+
+const config = require('../../../config/pg.config');
+
+class ProjectService extends Service {
+  constructor(broker) {
+    super(broker);
+    
+    this.parseServiceSchema({
+      name: 'project.model',
+      mixins: [DbService],
+      adapter: new SqlAdapter(config.dsn),
+      model: {
+        name: 'project',
+        define: {
+          id: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+          },
+          name: {
+            type: Sequelize.STRING, unique: true, allowNull: false,
+          },
+          token: {
+            type: Sequelize.STRING, unique: false, allowNull: false,
+          },
+          status: {
+            type: Sequelize.INTEGER, allowNull: true, defaultValue: null,
+          },
+          created_at: {
+            type: Sequelize.DATE, defaultValue: Sequelize.NOW,
+          },
+          updated_at: {
+            type: Sequelize.DATE, defaultValue: Sequelize.NOW, allowNull: true,
+          },
+        },
+        options: {
+          underscored: true,
+          indexes: [
+            {
+              unique: false,
+              fields: ['token'],
+            }, {
+              unique: false,
+              fields: ['status'],
+            }, {
+              unique: false,
+              fields: ['created_at'],
+            }],
+        },
+      },
+      actions: {
+        // ====
+        save: {
+          params: {
+            // name: {type: 'string'},
+            // token: {type: 'string'},
+          },
+          /**
+           * Save project
+           * @param {Context} ctx
+           * @returns {Promise<{status: boolean}>}
+           * @route project.model.save
+           */
+          async handler(ctx) {
+            let {name, value} = ctx.params;
+            
+            // todo save project
+            
+          },
+        },
+        
+      },
+    });
+    
+  }
+}
+
+module.exports = ProjectService;
