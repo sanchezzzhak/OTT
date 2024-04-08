f5fc3d22c030662d41d9dd9f714b3ca121b26dc3
Alexsandr Tutik <sanchezzzhak@yandex.ru>
2024-04-08 18:47:02 +0300

    1

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
-// const pathRegexp = require('path-to-regexp');
 require('dotenv').config({ path: __dirname + '/../.env' });
 
-const {resolve} = require('path');
-const {ServiceBroker} = require('moleculer');
+const { ServiceBroker } = require('moleculer');
+const { resolve } = require('path');
 // configs init
 const moleculerConfig = require('../config/moleculer.config');
 // microservices init
 const broker = new ServiceBroker(moleculerConfig);
 
 broker.loadServices(
-  resolve(__dirname, 'services'), '*.service.js',
-);
-broker.loadServices(
-    resolve(__dirname, 'services/models'), '*.service.js',
+  resolve(__dirname, 'services'), '*.service.js'
 );
 
-broker.start().then(() => {
-
-});
+broker
+  .start()
+  .then(() => {
+    broker.logger.info('✔ Broker start completed');
+    broker.logger.info('✔ OTT server started');
+    broker.logger.info('✔ Good luck');
+  }).catch(e => {
+    broker.logger.error(e);
+  });
--- config/moleculer.config.js
+++ config/moleculer.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  transporter: process.env.MOLECULAR_TRANSPORT ?? 'tcp'
+  transporter: process.env.MOLECULAR_TRANSPORT ?? 'TCP',
+  nodeID: 'OTT',
+  registry: {
+    strategy: process.env.REGISTRY_STRATEGY ?? "RoundRobin",
+    preferLocal: process.env.REGISTRY_PREFER_LOCAL ?? false,
+  },
+  logger: console
 };
