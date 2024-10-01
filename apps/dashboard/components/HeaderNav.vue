8a22e8a08cafe4c2381d67eba1ff5b56856de257
Alexsandr Tutik <sanchezzzhak@yandex.ru>
2024-10-01 14:05:24 +0300

    reformat

--- apps/dashboard/components/HeaderNav.vue
+++ apps/dashboard/components/HeaderNav.vue
@@ -676,9 +676,7 @@
               </li>
             </ul>
           </div>
-
         </li>
-
 
         <li class="nav-item dropdown">
           <a class="nav-link lh-1 pe-0"
@@ -712,38 +710,6 @@
       </ul>
     </div>
   </nav>
-  <!--    <nav class="navbar navbar-expand-md navbar-dark rounded-top navigation&#45;&#45;header">-->
-  <!--        <div class="container-fluid">-->
-  <!--            <router-link :to="{name: 'Home'}" class="navbar-brand">OTT</router-link>-->
-  <!--            <button class="navbar-toggler"-->
-  <!--                    type="button"-->
-  <!--                    data-bs-toggle="collapse"-->
-  <!--                    data-bs-target="#navbarSupportedContent"-->
-  <!--                    aria-controls="navbarSupportedContent"-->
-  <!--                    aria-expanded="false"-->
-  <!--                    aria-label="Toggle navigation"-->
-  <!--            ><span class="navbar-toggler-icon"></span></button>-->
-  <!--            <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-  <!--                <ul class="navbar-nav me-auto mb-2 mb-lg-0">-->
-  <!--                    <li class="nav-item" v-if="isAuth()">-->
-  <!--                        <router-link :to="{name: 'Dashboard'}" class="nav-link"> {{$t('dashboard')}}</router-link>-->
-  <!--                    </li>-->
-  <!--                    <li class="nav-item"></li>-->
-  <!--                </ul>-->
-
-
-  <!--                <div class="d-flex" v-if="!isAuth()">-->
-  <!--                    <router-link :to="{name: 'SingIn'}" class="nav-link btn btn-primary text-white"> {{$t('sing_in')}}-->
-  <!--                    </router-link>-->
-  <!--                </div>-->
-  <!--                <div class="d-flex" v-else>-->
-  <!--                    <router-link :to="{name: 'Logout'}" class="nav-link btn btn-outline-dark text-white">{{$t('exit')}}-->
-  <!--                    </router-link>-->
-  <!--                </div>-->
-
-  <!--            </div>-->
-  <!--        </div>-->
-  <!--    </nav>-->
 </template>
 
 <style scoped="scss">
